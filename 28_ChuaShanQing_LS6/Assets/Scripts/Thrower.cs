39351b2c982e8aa8984ece4c481f56290d24022d
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-05-24 22:58:04 +0800

    Page 11

---
+++ 28_ChuaShanQing_LS6/Assets/Scripts/Thrower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    private float timebtwshots;
+    public float starttimebtwshots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timebtwshots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timebtwshots -= starttimebtwshots;
+        }
+
+        else
+        {
+            timebtwshots -= Time.deltaTime;
+        }
+    }
+}
