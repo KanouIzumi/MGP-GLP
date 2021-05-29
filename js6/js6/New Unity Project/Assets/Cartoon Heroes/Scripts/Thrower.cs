27a3035a63167f8b6ade546b2e9ba685009b10ce
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-05-29 21:42:32 +0800

    Yes

---
+++ js6/js6/New Unity Project/Assets/Cartoon Heroes/Scripts/Thrower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    private float timebtwshots;
+    public float starttimebtwshots;
+
+    public GameObject projectile;
+    public Transform Player;
+
+    // start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timebtwshots <= 0)
+
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timebtwshots = starttimebtwshots;
+        }
+
+        else
+        {
+            timebtwshots -= Time.deltaTime;
+        }
+    }
+}
