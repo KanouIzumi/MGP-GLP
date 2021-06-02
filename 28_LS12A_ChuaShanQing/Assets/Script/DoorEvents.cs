37f85f2692c520bb392438c3016bb1ae6a39b92f
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-06-02 17:51:07 +0800

    Finish

---
+++ 28_LS12A_ChuaShanQing/Assets/Script/DoorEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Referencing to GameEvents Script
+        GameEvents.game_event.OnDorrwayTriggerEnter += OnDoorWayOpen;
+
+        GameEvents.game_event.OnDorrwayTriggerExit += OnDoorWayClose;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDoorWayOpen()
+    {
+        this.transform.position = new Vector3(0.43f, 3, 0);
+    }
+
+    private void OnDoorWayClose()
+    {
+        this.transform.position = new Vector3(0.43f, 0, 0);
+    }
+}
---
+++ 28_LS12A_ChuaShanQing/Assets/Script/GameEvents.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents game_event;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        //creating instance
+        game_event = this;
+    }
+
+    //Event Actin
+    public event Action OnDorrwayTriggerEnter;
+    public event Action OnDorrwayTriggerExit;
+
+    //Trigger Method
+    public void DoorwayTriggerEnter()
+    {
+        if(OnDorrwayTriggerEnter != null)
+        {
+            OnDorrwayTriggerEnter();
+        }
+    }
+
+    public void DoorwayTriggerExit()
+    {
+        if (OnDorrwayTriggerExit != null)
+        {
+            OnDorrwayTriggerExit();
+        }
+    }
+
+}
