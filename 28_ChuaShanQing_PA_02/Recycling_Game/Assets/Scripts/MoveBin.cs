282e5a6e0b9c4f442b66ff4fd3ff00b8c30ffebb
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-06-09 10:10:06 +0800

    Pa02

---
+++ 28_ChuaShanQing_PA_02/Recycling_Game/Assets/Scripts/MoveBin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveBin : MonoBehaviour
+{
+    public int distance = 10;
+    public Text itemsUpdate;
+    // Start is called before the first frame update
+
+    public int itemsCollected;
+
+
+  void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+    }
+
+
+    private void Update()
+    {
+
+        if(itemsCollected>=5)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        
+        if (other.gameObject.tag == "Reduce")
+        {
+         
+            SceneManager.LoadScene("GameLose");
+
+        }
+
+        if (other.gameObject.tag == "Recycle")
+        {
+            Destroy(other.gameObject);
+            itemsCollected++;
+            itemsUpdate.text = "Recycled items " + itemsCollected.ToString();
+
+        }
+    }
+
+
+}
---
+++ 28_ChuaShanQing_PA_02/Recycling_Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+
+    
+    public GameObject SpawnedObjects;
+    private float spawntime = 1;
+    private float spawndelay = 0.8f;
+    private int randomobjects;
+    float positionX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawntime, spawndelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObject()
+    {
+    
+        positionX = Random.Range(-9f,9f);
+        this.transform.position = new Vector3(positionX, transform.position.y, transform.position.z);
+        Instantiate(SpawnedObjects, transform.position, transform.rotation);
+    }
+}
