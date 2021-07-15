64109626950c95cb7da7aabe8b1b8c3aaee06aea
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-07-15 22:39:16 +0800

    Yes

---
+++ LS14_AR_ChuaShanQing/Assets/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour
+{
+    public Text ScoreText;
+    int Score;
+
+
+   // private AudioSource audioSource;
+    //public AudioClip[] AudioClipBGMArr;
+
+    public GameObject arCamera;
+
+    public GameObject smoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            if (hit.transform.name == "Blue ballon(Clone)" || hit.transform.name == "Purple ballon(Clone)" || hit.transform.name == "Red ballon(Clone)")
+            {
+                Destroy(hit.transform.gameObject);
+                //audioSource.PlayOneShot(AudioClipBGMArr[0]);
+                Score++;
+
+                ScoreText.GetComponent<Text>().text = "Score: " + Score.ToString();
+
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+
+}
---
+++ LS14_AR_ChuaShanQing/Assets/Scripts/Spawning_Scripts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning_Scripts : MonoBehaviour
+{
+
+    public Transform[] SpawnPoints;
+    public GameObject[] Balloons;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(6);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(Balloons[i], SpawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+
+    }
+
+}
