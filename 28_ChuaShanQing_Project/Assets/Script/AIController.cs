158a13c04ed66accfeea24d3dc0a5639532c1eda
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-08-07 22:17:58 +0800

    Doing Ms Zhou Project

---
+++ 28_ChuaShanQing_Project/Assets/Script/AIController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public float walkSpeed;
+
+    [HideInInspector]
+    public bool mustPatrol;
+    private bool mustFlip;
+
+    public Rigidbody2D rb;
+    public Transform groundChecksPos;
+
+    public LayerMask groundLayer;
+
+    public Collider2D bodyCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mustPatrol)
+        {
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(mustPatrol)
+        {
+            mustFlip = !Physics2D.OverlapCircle(groundChecksPos.position, 0.1f , groundLayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if (mustFlip || bodyCollider.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed + Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
---
+++ 28_ChuaShanQing_Project/Assets/Script/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = Target.position + offset;
+    }
+
+}
